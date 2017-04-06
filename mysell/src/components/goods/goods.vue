1f4674be2572c406e775d4a53a660f35cbc7f64a
razerpan <83943867@qq.com>
2017-04-06 18:37:30 +0800

    xx

--- mysell/src/components/goods/goods.vue
+++ mysell/src/components/goods/goods.vue
@@ -1,9 +1,122 @@
 <template>
-  <div class="goods">wo shi goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text"><span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}</span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon">
+                <img :src="food.icon" alt="">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span>月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span>￥{{food.price}}</span>
+                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
-  export default{};
+  const ERR_OK = 0;
+
+  export default{
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: []
+      };
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.goods = response.data;
+          console.log(this.goods);
+        }
+      });
+    }
+  };
 </script>
 <style lang="scss" rel="stylesheet/scss">
+  @import "../../common/scss/mixin";
 
+  .goods {
+    display: flex;
+    position: absolute;
+    width: 100%;
+    top: size(363);
+    bottom: size(92);
+    overflow: hidden;
+    .menu-wrapper {
+      flex: 0 0 size(160);
+      width: size(160);
+      background-color: #f3f5f7;
+      .menu-item{
+        display: table;
+        height: size(108);
+        width: size(112);
+        line-height: size(28);
+        margin: 0 auto;
+        .icon{
+          display: inline-block;
+          vertical-align: top;
+          width: size(24);
+          height: size(24);
+          margin-right: size(4);
+          background-size: size(24) size(24);
+          background-repeat: no-repeat;
+          &.decrease {
+            @include bg-image('decrease_3');
+          }
+          &.discount {
+            @include bg-image('discount_3');
+          }
+          &.guarantee {
+            @include bg-image('guarantee_3');
+          }
+          &.invoice {
+            @include bg-image('invoice_3');
+          }
+          &.special {
+            @include bg-image('special_3');
+          }
+        }
+        .text{
+          display: table-cell;
+          width: size(112);
+          vertical-align: middle;
+          font-size: size(24);
+          border-bottom: size(1) solid rgba(7,17,27,0.2);
+        }
+      }
+    }
+    .foods-wrapper {
+      flex: 1;
+    }
+  }
 </style>
